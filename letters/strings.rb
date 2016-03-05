f18f161232cfe175c1dd3f1ce36783b75490f7b0
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-03-05 09:57:11 -0800

    letters

---
+++ letters/strings.rb
@@ -0,0 +1,35 @@
+# 'Hello.'
+# 'Ruby rocks.'
+# '5 is my favorite number ... what is yours?'
+# 'Snoopy says #%^?&*@! when he stubs his toe.'
+# '      '
+# ''
+
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+puts 'I like' + ' apple pie' # need that space
+
+puts 'blink ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12  +  12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+# TypeError puts '12' + 12
+# TypeError puts '2' * '5'
+
+# puts 'Betty' + 12
+# puts 'Fred' * 'John'
+
+puts 'pig' * 5
+# TypeError puts 5 * 'pig'
+
+puts 'You\'re swell!'
+# or
+puts "You're swell!"
