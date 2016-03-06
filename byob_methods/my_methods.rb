fa5ff93e665f8cbf2c5f4fb61f9026396d8eee53
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-03-05 20:44:53 -0800

    trying to work out english number

--- byob_methods/my_methods.rb
+++ byob_methods/my_methods.rb
@@ -1,3 +1,12 @@
+puts 'Hello, and thank you for taking the time to'
+puts 'help me with this experiment. My experiment'
+puts 'has to do with the way people feel about'
+puts 'Mexican food. Just think about Mexican food'
+puts 'and try to answer every question honestly,'
+puts 'with either a "yes" or a "no". My experiment'
+puts 'has nothing to do with bed-wetting.'
+puts
+
 goodAnswer = false
 while (not goodAnswer)
   puts 'Do you like eating tacos?'
@@ -8,3 +17,314 @@ while (not goodAnswer)
     puts 'Please answer "yes" or "no".'
   end
 end
+
+goodAnswer = false
+while (not goodAnswer)
+  puts 'Do you wet the bed?'
+  answer = gets.chomp.downcase
+  if (answer == 'yes' or answer == 'no')
+    goodAnswer = true
+    if answer == 'yes'
+      wetsBed = true
+    elsif answer == 'no'
+      wetsBed = true
+    end
+  else
+    puts 'Please answer "yes" or "no".'
+  end
+end
+
+goodAnswer = false
+while (not goodAnswer)
+  puts 'Do you like eating chimichangas?'
+  answer = gets.chomp.downcase
+  if (answer == 'yes' or answer == 'no')
+    goodAnswer = true
+  else
+    puts 'Please answer "yes" or "now".'
+  end
+end
+
+puts 'Just a few more questions...'
+
+goodAnswer = false
+while (not goodAnswer)
+  puts 'Do you like eating sopapillas?'
+  answer = gets.chomp.downcase
+  if (answer == 'yes' or answer == 'no')
+    goodAnswer = true
+  else
+    puts 'Please answer "yes" or "no".'
+  end
+end
+
+puts
+puts 'DEBRIEFING:'
+puts 'Thank you for taking the time to help with'
+puts 'this experiment. In fact, this experiment'
+puts 'has nothing to do with Mexican food. It is'
+puts 'an experiment about bed-wetting. The Mexican'
+puts 'food was just there to catch you off-guard'
+puts 'in the hopes that you would answer more'
+puts 'honestly. Thanks again.'
+puts
+puts wetsBed
+
+def doubleThis num
+  numTimes2 = num*2
+  puts num.to_s+' doubled is '+numTimes2.to_s
+end
+
+doubleThis 44
+
+# will throw an error -- puts numTimes2.to_s
+
+def littlePest var
+  var = nil
+  puts 'HAHA! I ruined your variable!'
+end
+
+var = 'You can\'t even touch my variable!'
+littlePest var
+puts var
+
+returnVal = puts 'This puts returned:'
+puts returnVal
+
+def sayMoo numberOfMoos
+  puts 'mooooooo.....'*numberOfMoos
+  'yellow submarine'
+end
+
+x = sayMoo 2
+puts x
+
+def ask question
+  goodAnswer = false
+  while (not goodAnswer)
+    puts question
+    reply = gets.chomp.downcase
+
+    if (reply == 'yes' or reply == 'no')
+      goodAnswer = true
+      if reply == 'yes'
+        answer = true
+      else
+        answer = false
+      end
+    else
+      puts 'Please answer "yes" or "no".'
+    end
+  end
+
+  answer
+end
+
+puts 'Hello ...'
+puts
+
+ask 'Do you like eating tacos?'
+ask 'Do you like eating burritos?'
+wetsBed = ask 'Do you wet the bed?'
+ask 'Do you like eating chimichangas?'
+ask 'Do you like churros?'
+ask 'Yadda Yadda Yadda'
+
+# ...
+
+def englishNumber number
+  if number < 0
+    return 'Please enter a number zero or greater.'
+  end
+  if number > 100
+    return 'Please enter a number 100 or lesser.'
+  end
+
+  numString = ''
+
+  left = number
+  write = left/100
+  left = left - write * 100
+
+  if write > 0
+    return 'one hundred'
+  end
+
+  write = left/10
+  left = left - write*10
+
+  if write > 0
+    if write == 1
+      if left == 0
+        numString = numString + 'ten'
+      elsif left == 1
+        numString = numString + 'eleven'
+      elsif left == 2
+        numString = numString + 'twelve'
+      elsif left == 3
+        numString = numString + 'thirteen'
+      elsif left == 4
+        numString = numString + 'fourteen'
+      elsif left == 5
+        numString = numString + 'fifteen'
+      elsif left == 6
+        numString = numString + 'sixteen'
+      elsif left == 7
+        numString = numString + 'seventeen'
+      elsif left == 8
+        numString = numString + 'eighteen'
+      elsif left == 9
+        numString = numString + 'nineteen'
+      end
+
+      left = 0
+    elsif write == 2
+      numString = numString + 'twenty'
+    elsif write == 3
+      numString = numString + 'thirty'
+    elsif write == 4
+      numString = numString + 'forty'
+    elsif write == 5
+      numString = numString + 'fifty'
+    elsif write == 6
+      numString = numString + 'sixty'
+    elsif write == 7
+      numString = numString + 'seventy'
+    elsif write == 8
+      numString = numString + 'eighty'
+    elsif write == 9
+      numString = numString + 'ninety'
+    end
+
+    if left > 0
+      numString = numString + '-'
+    end
+  end
+
+  write = left
+  left = 0
+
+  if write > 0
+    if write == 1
+      numString = numString + 'one'
+    elsif write == 2
+      numString = numString + 'two'
+    elsif write == 3
+      numString = numString + 'three'
+    elsif write == 4
+      numString = numString + 'four'
+    elsif write == 5
+      numString = numString + 'five'
+    elsif write == 6
+      numString = numString + 'six'
+    elsif write == 7
+      numString = numString + 'seven'
+    elsif write == 8
+      numString = numString + 'eight'
+    elsif write == 9
+      numString = numString + 'nine'
+    end
+  end
+
+  if numString == ''
+    return 'zero'
+  end
+
+  numString
+end
+
+puts englishNumber(0)
+puts englishNumber(1)
+puts englishNumber(19)
+puts englishNumber(99)
+puts englishNumber(88)
+
+def englishNumber2 number
+  if number < 0  # No negative numbers.
+    return 'Please enter a number that isn\'t negative.'
+  end
+  if number == 0
+    return 'zero'
+  end
+
+  # No more special cases! No more returns!
+
+  numString = ''  # This is the string we will return.
+
+  onesPlace = ['one',     'two',       'three',    'four',     'five',
+               'six',     'seven',     'eight',    'nine']
+  tensPlace = ['ten',     'twenty',    'thirty',   'forty',    'fifty',
+               'sixty',   'seventy',   'eighty',   'ninety']
+  teenagers = ['eleven',  'twelve',    'thirteen', 'fourteen', 'fifteen',
+               'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+  # "left" is how much of the number we still have left to write out.
+  # "write" is the part we are writing out right now.
+  # write and left... get it?  :)
+  left  = number
+  write = left/100          # How many hundreds left to write out?
+  left  = left - write*100  # Subtract off those hundreds.
+
+  if write > 0
+    # Now here's a really sly trick:
+    hundreds  = englishNumber2 write
+    numString = numString + hundreds + ' hundred'
+    # That's called "recursion". So what did I just do?
+    # I told this method to call itself, but with "write" instead of
+    # "number". Remember that "write" is (at the moment) the number of
+    # hundreds we have to write out. After we add "hundreds" to
+    # "numString", we add the string ' hundred' after it.
+    # So, for example, if we originally called englishNumber with
+    # 1999 (so "number" = 1999), then at this point "write" would
+    # be 19, and "left" would be 99. The laziest thing to do at this
+    # point is to have englishNumber write out the 'nineteen' for us,
+    # then we write out ' hundred', and then the rest of
+    # englishNumber writes out 'ninety-nine'.
+
+    if left > 0
+      # So we don't write 'two hundredfifty-one'...
+      numString = numString + ' '
+    end
+  end
+
+  write = left/10          # How many tens left to write out?
+  left  = left - write*10  # Subtract off those tens.
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      # Since we can't write "tenty-two" instead of "twelve",
+      # we have to make a special exception for these.
+      numString = numString + teenagers[left-1]
+      # The "-1" is because teenagers[3] is 'fourteen', not 'thirteen'.
+
+      # Since we took care of the digit in the ones place already,
+      # we have nothing left to write.
+      left = 0
+    else
+      numString = numString + tensPlace[write-1]
+      # The "-1" is because tensPlace[3] is 'forty', not 'thirty'.
+    end
+
+    if left > 0
+      # So we don't write 'sixtyfour'...
+      numString = numString + '-'
+    end
+  end
+
+  write = left  # How many ones left to write out?
+  left  = 0     # Subtract off those ones.
+
+  if write > 0
+    numString = numString + onesPlace[write-1]
+    # The "-1" is because onesPlace[3] is 'four', not 'three'.
+  end
+
+  # Now we just return "numString"...
+  numString
+end
+
+
+puts englishNumber2(0)
+puts englishNumber2(5)
+puts englishNumber2(10)
+puts englishNumber2(200)
