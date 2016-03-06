b93c88ff4b47637fec25e43e961ec6420c5fbdd0
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-03-05 17:24:57 -0800

    finished sort_me

--- arrays/make_a_note.rb
+++ arrays/make_a_note.rb
@@ -8,19 +8,22 @@ end
 
 # puts notes.sort
 
+def swap(arr, i)
+  
+  arr
+end
+
 def sort_me(arr)
-  arr.each_with_index do |element, i|
-    break if arr[i] == arr.last
-    p arr
-    puts "comparing #{arr[i]} with #{arr[i + 1]}"
+  for i in 0...(arr.length - 1)
+    return arr if arr.length == 1
     if arr[i] > arr[i + 1]
       arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      sort_me(arr)
     end
-    arr = arr[0..i] << sort_me(arr[i + 1..-1])
   end
   arr
 end
 
 p notes
-notes = sort_me(notes).flatten
+notes = sort_me(notes)
 p "Notes: #{notes}"
