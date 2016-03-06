1165fbfdc70dca6107a4c9466fa64086275ad544
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-03-05 17:32:21 -0800

    starting 'write your own methods' chapter

---
+++ byob_methods/my_methods.rb
@@ -0,0 +1,10 @@
+goodAnswer = false
+while (not goodAnswer)
+  puts 'Do you like eating tacos?'
+  answer = gets.chomp.downcase
+  if (answer == 'yes' or answer == 'no')
+    goodAnswer = true
+  else
+    puts 'Please answer "yes" or "no".'
+  end
+end
